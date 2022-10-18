9e8f85a394f5f6f4739185537711afd0edb820c6
zyxx66 <914731317@qq.com>
2022-10-18 17:29:01 +0900

    編集

--- create_graph.py
+++ create_graph.py
@@ -5,3 +5,4 @@ for file_path in os.popen('rclone ls gdrive_taka:').read().split('\n'):
         # print('偏光測定器 データ'+file_path.split('データ')[1])
         gdrive_path = 'gdrive_taka:偏光測定器\u3000データ' + file_path.split('データ')[1]
         os.system('rclone copy %s /home/pi/Documents/test/filetest/ -P' % (gdrive_path))
+
--- test2.py
+++
@@ -1,3 +0,0 @@
-import os
-for file in os.walk('/home/pi/Documents/test/bw/'):
-    print(file)
