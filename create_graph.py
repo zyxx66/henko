571e1816275ddf47a84d62cb2eead1e3bbfee064
zyxx66 <914731317@qq.com>
2022-10-19 17:31:16 +0900

    2022年10月19日17:31:13

--- create_graph.py
+++ create_graph.py
@@ -1,5 +1,4 @@
 import os
-
 for file_path in os.popen('rclone ls gdrive_taka:').read().split('\n'):
     if '-n' in file_path and file_path.endswith('.csv'):
         # print('偏光測定器 データ'+file_path.split('データ')[1])
--- test1.py
+++ test1.py
@@ -1,6 +1,3 @@
 k = input('input a number:\n')
 if k == 1:
     pass
-
-# this it a test file
-# test how to get programming to raspberry
