aee489df676c914049462944ee4ae694868a54ad
zyxx66 <914731317@qq.com>
2022-10-26 12:12:47 +0900

    Update test_infrared_sensor.py

--- test_infrared_sensor.py
+++ test_infrared_sensor.py
@@ -62,7 +62,7 @@ time_local = time.strftime('%Y-%m-%d', time.localtime(time.time()))
 csv_file = '/home/pi/henko/result/%s.csv' % time_local
 file = open(csv_file, 'a')
 file.write(time_local + ',,,' + time.strftime('%H:%M:%S', time.localtime(
-    time.time())) + '\n' + 'angle(°),henko(LUX),CH0,CH1,LUX1,LUX2\n')
+    time.time())) + '\n' + 'angle,henko(LUX),CH0,CH1,LUX1,LUX2\n')
 
 
 def initTSL2572():
