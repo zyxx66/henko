c71102a4551754ffd40a348b5b8584fc1d6c982b
zyxx66 <914731317@qq.com>
2023-01-10 16:44:16 +0900

    update

--- time.py
+++ time.py
@@ -1,6 +1,8 @@
 import requests
 import os
 import rclone_method
+import time
+import calendar
 
 ExchangeMounth = {'Jan':1,'Feb':2,'Mar':3,'Apr':4,'May':5,'Jun':6,'Jul':7,'Aug':8,'Sep':9,'Oct':10,'Nov':11,'Dec':12}
 
@@ -23,16 +25,28 @@ while True:
             minute_now = net_date[20:22]
             second_now = net_date[23:25]
 
-       
-            if hour_now < 10:
-              hour_now = '0' + str(hour_now)
-            elif hour_now >= 10 & hour_now < 24:
-              hour_now = str(hour_now)
-            elif hour_now > 24:
-              hour_now -= 24
-              hour_now = '0' + str(hour_now[0])
+            # 今月はday_in_mounth日ある
+            days_in_mounth = calendar.monthrange(int(year_now), int(mounth_now))[1]
 
-            time_now = hour_now + ":" + minute_now + ":" + second_now
+            if hour_now < 10:
+                hour_now = '0' + str(hour_now)
+            # もし時が24超えるとの操作
+            elif hour_now >= 24:
+                hour_now -= 24
+                day_now = int(day_now) + 1
+                hour_now = '0' + str(hour_now)
+
+            # 1月32日みたいな結果が出る場合の操作
+            if day_now > days_in_mounth:
+                day_now -= days_in_mounth
+                mounth_now = int(mounth_now) + 1
+
+            # 2022年13月みたいな結果が出る場合の操作
+            if mounth_now > 12:
+                mounth_now -= 12
+                year_now = int(year_now) + 1
+
+            time_now = str(hour_now) + ":" + minute_now + ":" + second_now
           
             date_now = year_now + "-" + str(mounth_now) + "-" + day_now + " " + time_now
 
@@ -44,4 +58,5 @@ while True:
             break
     except Exception as e:
         print(e)
+        time.sleep(3)
         continue
--- with_excel/test4.py
+++
@@ -1,8 +0,0 @@
-import create_graph as cg
-xlsx_file = 'C:/Users/zyxx/Desktop/sumup_2.xlsx'
-graph = cg.create_graph(xlsx_file, 'sumup_2')
-
-for i in range(24):
-    graph.create_scatter(graph.load(),'雲量(％)','照度(Lux)','','B'+str(94+i*5),[3+3*i,30,3+3*i,93,2+3*i,30,2+3*i,93])
-
-graph.save(graph.load())
