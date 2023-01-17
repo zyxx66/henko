5d9f024e655ed1016460824d920d3315762c7e27
zyxx66 <914731317@qq.com>
2023-01-17 11:17:31 +0900

    2023年1月16日12:18:53

--- rclone/rclone_method.py
+++ rclone/rclone_method.py
@@ -35,27 +35,23 @@ def date_time(date_start, date_end, data):
         data.append(start.date())
         start += step
 
-for name in os.popen('rclone ls %s' % gdrive_code_path).read().split('\n'):
-    if name != '':
-        gdrive_code_name.append(name.strip().split(' ')[1])
 
-
-# ファイルを更新する　メソッド
+# ファイルを更新する
 def update(source_path, target_path):
     os.system('rclone sync %s %s -P' % (source_path, target_path))
 
 
-# ファイルをアップロードする　メソッド
+# ファイルをアップロードする
 def upload(source_path, target_path):
     os.system('rclone copy %s %s -P' % (source_path, target_path))
 
 
-# ファイルを削除する　メソッド
+# ファイルを削除する
 def delete(source_path):
     os.system('rclone delete %s' % (source_path))
 
 
-# -------------------line通知機能--------------------------
+# line通知機能
 def line_send_message(message):
     data = {'message': message}
     requests.post(line_notify_url, headers=headers, data=data)
